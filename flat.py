1dc7bef474d59aba80e021f210f6b648f2f89c20
zmcclendon <zmcclendon216@gmail.com>
2017-11-14 21:18:23 -0600

    Add files via upload

--- flat.py
+++ flat.py
@@ -1,14 +1,16 @@
 import csv
 import numpy
+import sys
 
 flatShit = []
 
-with open('stuff.csv', 'rU') as csvfile:
+with open(sys.argv[1]+".csv", 'rU') as csvfile:
     reader = csv.reader(csvfile, delimiter='\t', quotechar='|')
     x = list(reader)
     multiplier = len(x)/10
     for column in range(len(x[1])):
         for item in range(10):
             flatShit.append(x[item*multiplier][column])
+    a = numpy.asarray(flatShit)
 
-print flatShit
+numpy.savetxt(sys.argv[1]+"_out.csv", a, delimiter=",", fmt="%s")
